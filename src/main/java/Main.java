8ca9ed275100e8bce94c79080a5f42464074520d
Jai Kherajani <jai.kherajani19@gmail.com>
2024-12-18 21:44:01 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -11,7 +15,7 @@ public class Main {
 
      try {
        ServerSocket serverSocket = new ServerSocket(4221);
-         Socket clientSocket = null;
+       Socket clientSocket = null;
 
        // Since the tester restarts your program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
@@ -19,8 +23,17 @@ public class Main {
 
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-
+       InputStream inputStream = clientSocket.getInputStream();
+       BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+       String input = reader.readLine();
+       String address[] = input.split(" ");
+       //System.out.println(Arrays.toString(address));
+       String endpoint = address[1];
+       //System.out.println("endpoint is : " + endpoint);
+       if ("/".equals(endpoint))
+         clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+       else
+         clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
